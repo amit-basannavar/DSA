9f13e38458a7c24b1c283ae65490e28bef813dfb
amit-basannavar <amitbasannavar65@gmail.com>
2025-07-25 19:38:19 +0530

    Count Count Of Each Character in a given string

---
+++ src/main/java/com/practice/array/strings/CountOfEachCharacterInGivenString.java
@@ -0,0 +1,17 @@
+package com.practice.array.strings;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountOfEachCharacterInGivenString {
+    public static void main(String[] args) {
+        String input = "Om Shree Guru Raghavendray Namaha";
+        Map<String, Long> output = Arrays.stream(input.split(""))
+                .filter(x->!x.isEmpty())
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+        System.out.println(output);
+    }
+}
